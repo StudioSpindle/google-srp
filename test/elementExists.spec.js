69fc91d123032b81ae4d8b889c1af0214db16dd8
Vledder <remi@studiospindle.nl>
2019-02-27 20:38:37 +0100

    increase variable scope in test

--- test/elementExists.spec.js
+++ test/elementExists.spec.js
@@ -2,17 +2,16 @@ const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 import { describe } from 'mocha';
 const expect = require('chai').expect;
+
 const elementExists = require('../src/helpers/elementExists').default;
 
 describe('elementExists...', function() {
-  let mockElement, falseElement;
-
-  before(function() {
-    const dom = new JSDOM(`<!DOCTYPE html><div></div>`);
-    mockElement = dom.window.document.querySelector('div');
-    falseElement = dom.window.document.querySelector('p');
+  beforeEach(function() {
+    const jsdom = require('jsdom');
+    const { JSDOM } = jsdom;
+    const { window } = new JSDOM('<!doctype html><html><div></div></html>');
+    global.window = window;
   });
-
   it(`should be a function`, function() {
     expect(elementExists).to.be.a('function');
   });
@@ -20,9 +19,11 @@ describe('elementExists...', function() {
     expect(elementExists()).to.be.false;
   });
   it(`should return 'false' if the element doesn't exist.`, function() {
+    const falseElement = global.window.document.querySelector('p');
     expect(elementExists(falseElement)).to.be.false;
   });
-  it(`should return 'true' if the element does exist`, function() {
+  it(`should return 'true' if the element does exist.`, function() {
+    const mockElement = global.window.document.querySelector('div');
     expect(elementExists(mockElement)).to.be.true;
   });
 });
